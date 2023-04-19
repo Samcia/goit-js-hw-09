dc70ee11c1cc40b5a472f504fbab9a451c19a749
Samcia <117597986+Samcia@users.noreply.github.com>
2023-04-19 10:10:18 +0000

    Deploying to gh-pages from @ Samcia/goit-js-hw-09@0aebb25c64b8497976e4ed0fe4999758a60d1dba 🚀

---
+++ 01-color-switcher.b93e69bb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=0;document.getElementById;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.b93e69bb.js.map
